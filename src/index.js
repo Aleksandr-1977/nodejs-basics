d078634363b6f2ed22d8367bcff319199e914396
Alex Vasylenko <vasilenkobox@gmail.com>
2025-04-28 21:40:30 +0300

    add write

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import fs from 'node:fs';
+import fs from 'node:fs/promises';
 const message = 'Hello world';
 function sum(a, b) {
   return a + b;
@@ -13,3 +13,17 @@ fs.readFile('text.txt', { encoding: 'utf-8' }, (err, data) => {
   }
   console.log(data);
 });
+fs.writeFile('write.txt', 'Hello,world!')
+  .then(() => {
+    console.log('File written successfully');
+  })
+  .catch((err) => {
+    console.error('Error writing file', err);
+  });
+fs.appendFile('write.txt', 'Bye,world!\n')
+  .then(() => {
+    console.log('File appended successfully');
+  })
+  .catch((err) => {
+    console.error('Error appending file', err);
+  });
