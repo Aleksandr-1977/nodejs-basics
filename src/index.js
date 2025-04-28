1f5b3fe65baf45bed5c166eb520404718b15386c
Alex Vasylenko <vasilenkobox@gmail.com>
2025-04-28 21:15:45 +0300

    add  fs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import fs from 'node:fs';
 const message = 'Hello world';
 function sum(a, b) {
   return a + b;
@@ -5,3 +6,10 @@ function sum(a, b) {
 const result = sum(2, 5);
 console.log(result);
 console.log(message);
+
+fs.readFile('text.txt', { encoding: 'utf-8' }, (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log(data);
+});
